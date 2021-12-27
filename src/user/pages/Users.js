6a46bcae8028876d826ea9ca736cc926e6daab07
hasibmiraz <hasibmiraz09@gmail.com>
2021-12-28 00:51:44 +0600

    Updating the readme file

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -7,7 +7,7 @@ const Users = () => {
       id: 'u1',
       name: 'Miraz',
       image:
-        'https://www2.deloitte.com/content/dam/Deloitte/nl/Images/promo_images/deloitte-nl-cm-digital-human-promo.jpg',
+        'https://media.istockphoto.com/photos/portrait-of-handsome-smiling-young-man-with-crossed-arms-picture-id1200677760?k=20&m=1200677760&s=612x612&w=0&h=JCqytPoHb6bQqU9bq6gsWT2EX1G5chlW5aNK81Kh4Lg=',
       places: 3,
     },
   ];
